6bd5056eba59e1022e005706f449289040a0b4bb
Bryan Bickel <briimcfly@gmail.com>
2023-07-16 16:13:42 -0700

    generate rows per hour and style based on past, current, or future.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,5 @@
 var today = dayjs();
-//Don't start until DOM fully loads
-$(window).on("load", function(){
-  $(`#currentDay`).text(today.format(`dddd, MMMM YYYY`))
+$(`#currentDay`).text(today.format(`dddd, MMMM YYYY`))
   // $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -22,7 +20,61 @@ $(window).on("load", function(){
     //
     // TODO: Add code to display the current date in the header of the page.
   // });
+
+//Hour Row
+var hourBlock = 
+$(`<div>`, {
+  class: "row time-block"
 })
+.append($(`<div>`, {
+    class: "col-2 col-md-1 hour text-center py-3"
+  }))
+.append($(`<textarea>`, {
+  class: "col-8 col-md-10 description",
+  rows: "3"
+}))
+.append($(`<button>`, {
+    class: "btn saveBtn col-2 col-md-1",
+    "aria-label": "save"
+  })
+  .append($(`<i>`, {
+    class: "fas fa-save",
+    "aria-hidden": "true"
+  }))
+  );
+
+  //Shift Start Time (in 0-23)
+  var shiftHour = 15;
+
+  //Current Hour (in 0-23)
+  var currentHour = today.hour();
+
+  //Total Shift Length
+  var shiftLength = 8;
 
 
+//Generate a row for each hour in Shift Length and apply Present, Past, or Future class
+for (i=0; i < shiftLength; i++){
+  console.log(shiftHour);
+  console.log(currentHour);
+  console.log(`-----------`);
+  hourBlock
+  .clone()
+  .appendTo(`#schedule`)
+  .children().eq(0).text(dayjs().hour(shiftHour).format(`h A`));
 
+  //if this row = current hour...
+  if(shiftHour == currentHour) {
+    $(`#schedule > div`).last().addClass("present");
+  }
+  //if this row = past hour...
+  else if (shiftHour < currentHour){
+    $(`#schedule > div`).last().addClass("past");
+  }
+  //if this row = future hour...
+  else {
+    $(`#schedule > div`).last().addClass("future");
+  }
+
+  shiftHour ++;
+}
